cc6b58da5f9922bd27642cca69c6cc0eda84bc1a
messnerj <messnerj@stanford.edu>
2019-11-26 21:11:48 -0800

    changed sweep parameters

--- run_sweep.sh
+++ run_sweep.sh
@@ -1,8 +1,8 @@
-for img_loss in 1 1e-1 1e-2
+for img_loss in 1 0.5
 do
-    for adv_loss in 1 1e-1 1e-2
+    for adv_loss in 4e-2 2e-2 1e-2
     do
-        for percept_loss in 1e-1 3e-2 1e-2
+        for percept_loss in 1e-1 6e-2 3e-2
         do
             echo "img_loss: $img_loss, adv_loss: $adv_loss, percept_loss: $percept_loss"
             CUDA_VISIBLE_DEVICES=1 python train.py --epochs 100 --img_loss $img_loss --adv_loss $adv_loss --percept_loss $percept_loss
